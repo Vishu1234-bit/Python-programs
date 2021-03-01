328c47ec9c56ca6ea48956ef83c9c8a8a18ec7c1
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-03-01 11:38:52 +0530

    Add files via upload
    Given an array a:
    the array b should be in such a way 
    b[0] =a[0]
    b[1] = a[-1]
    b[2] = a[1]
    b[3] = a[-2]
    and so on 
    if the resulting b array are in strictly ascending order return true else return false

---
+++ alternatingSort.py
@@ -0,0 +1,19 @@
+def alternatingSort(a):
+     b=[]
+     print(a)
+     print(a[::-1])
+     for i,j in zip(a,reversed(a)):
+         b.append(i)
+         b.append(j)
+    
+     b = b[0:len(a)]
+     print(b)
+     res = sorted(set(a),key = lambda i : a.count(i))
+     print(res)
+     if(b==res):
+         return True
+     else:
+         return False
+    
+a = [-92, -17, 71, 76, 54, -35]
+print(alternatingSort(a))
