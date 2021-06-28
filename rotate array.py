acb127e19eb8a2bfc6c604ffa960d4fbc07899fa
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-06-28 15:34:15 +0530

    Create rotate array.py
    Given an array, rotate the array to the right by k steps, where k is non-negative.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,4,5,6,7], k = 3
    Output: [5,6,7,1,2,3,4]
    Explanation:
    rotate 1 steps to the right: [7,1,2,3,4,5,6]
    rotate 2 steps to the right: [6,7,1,2,3,4,5]
    rotate 3 steps to the right: [5,6,7,1,2,3,4]
    Example 2:
    
    Input: nums = [-1,-100,3,99], k = 2
    Output: [3,99,-1,-100]
    Explanation: 
    rotate 1 steps to the right: [99,-1,-100,3]
    rotate 2 steps to the right: [3,99,-1,-100]
     
    
    Constraints:
    
    1 <= nums.length <= 105
    -231 <= nums[i] <= 231 - 1
    0 <= k <= 105
     
    
    Follow up:
    
    Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
    Could you do it in-place with O(1) extra space?

---
+++ rotate array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+        nums[:]=nums[::-1]
+        nums[k:len(nums)]=nums[k:len(nums)][::-1]
+        nums[0:k]=nums[0:k][::-1]  
+        
